59e862b204476b52f00afb9eac6fb4025bb98442
aymanm <ayman.yousif@gmail.com>
2018-05-06 11:58:42 +0400

    split download into 2 steps, 1) generate r obj file in temp, then click to download.

--- inst/shiny/ui-tab-finish.R
+++ inst/shiny/ui-tab-finish.R
@@ -1,9 +1,20 @@
-tabItem(tabName = "finishTab", 
+tabItem(tabName = "finishTab",
         fluidRow(
           column(12,
                  h3(strong("Download R Object")),
                  p("You can save the object at this point so that it can easily be loaded back in R for further analysis & exploration without having to rerun the computationally intensive steps performed above, or easily shared with collaborators."),
-                 downloadLink("downloadRObj","Download Seurat Object",class = "button button-3d button-block button-pill button-royal button-large",style = "width: 100%"))
+
+
+                 column(12,
+                        column(6,
+                               actionButton('generateSeuratFile','Generate Seurat Robj',class = "button button-3d button-block button-pill button-royal button-large")
+                        ),
+                        column(6,
+                               conditionalPanel("output.seuratFileExists",
+                                                downloadButton('downloadRObj','Download Seurat Obj',class = "button button-3d button-block button-pill button-action button-large"))
+                        )
+                 )
+                 )
         )
-        
+
 )
