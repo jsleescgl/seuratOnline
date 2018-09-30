b9f1de894e9a023c1780f11835c8b5c6d4cb44fc
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-09-30 15:38:37 +0300

    more packages

--- install-seurat.R
+++ install-seurat.R
@@ -1,4 +1,14 @@
 #!/usr/bin/env Rscript
 
+install.packages("shinydashboard")
+install.packages("shinyjs")
+install.packages("shinyBS")
+install.packages("shinycssloaders")
+install.packages('DT')
+
+#In (function (dep_name, dep_ver = NA, dep_compare = NA)  :
+#  Need Seurat == 2.3.0 but loaded version is 2.3.4
+
 library(devtools);
-devtools::install_github("aymanm/seuratOnline");
+devtools::load_all()
+# devtools::install_github("aymanm/seuratOnline");
